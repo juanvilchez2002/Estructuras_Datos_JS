b802413c3d908e44e117ccfd2b14011ed292b30d
Juan Vilchez <juanvilchez2002@gmail.com>
2021-05-14 00:03:36 -0500

    Eliminando elementos del Array

--- Array.js
+++ Array.js
@@ -20,6 +20,48 @@ class MyArray{
         this.length++;
         return this.data;
     }
+
+    //elimando el ultimo elemento del arreglo, emulando metodo pop()
+    pop(){
+        const lastItem = this.data[this.length-1];
+        delete this.data[this.length - 1];//elimina el ultimo elemento
+        this.length--;
+        return lastItem;
+    }
+
+    delete(index){
+        const item = this.data[index];
+        this.shiftIndex(index);
+        return item;
+    }
+
+    //genera cambios en el indice
+    shiftIndex(index){
+        for(let i = index; i < this.length - 1; i++){
+            this.data[i] = this.data[i+1];
+        }
+        delete this.data[this.length - 1];
+        this.length--;
+    }
+
+    //adicionando un elemento al inicio del array
+    unshift(item){
+        //generando un loop que va de mayor a menor
+        for(let i = this.length; i > 0; i--){
+            //reemplazando el valor de ultimo elemento al penultimo y asi sucesivamente
+            this.data[i] = this.data[i-1];
+        }
+        //adicionando el item al primer elemento del array
+        this.data[0] = item;
+        this.length++;//incrementando la longitud del array
+        return this.data
+    }
+
+    //eliminando el primer elemento del array
+    shift(){
+        this.shiftIndex(0);
+        return this.data;
+    }
 }
 
 //inicializando la clase
